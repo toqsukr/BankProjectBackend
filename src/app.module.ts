6c7c6e7eef844d9f55a91d9e45336e1a69d34c65
Wastard <Ilya.pakylov@gmail.com>
2023-10-17 12:23:56 +0300

    rename user route

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
-import { UserProfileModule } from './user-profile/user-profile.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [AuthModule, UserProfileModule],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user-profile/user-profile.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UserProfileService } from './user-profile.service';
-
-@Controller('user-profile')
-export class UserProfileController {
-  constructor(private readonly userProfileService: UserProfileService) {}
-}
