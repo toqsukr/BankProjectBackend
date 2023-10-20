60795a8e01181ef0b50501976518403014978047
Wastard <Ilya.pakylov@gmail.com>
2023-10-20 15:27:45 +0300

    set getUser by phone route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode } from '@nestjs/common'
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger'
+import { UserDto } from './user.dto'
 import { UserService } from './user.service'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  @Get()
+  @HttpCode(200)
+  @ApiOperation({ summary: 'User profile' })
+  @ApiBody({ type: UserDto })
+  @ApiResponse({ status: 200, description: 'User found successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid data' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  getUser(@Body() userDto: UserDto) {
+    return this.userService.getUser(userDto)
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { isValidPhoneNumber } from 'src/auth/auth.helper'
+import { PrismaService } from 'src/prisma.service'
+import { UserDto } from './user.dto'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUser(userDto: UserDto) {
+    if (!isValidPhoneNumber(userDto.phone))
+      throw new HttpException('Incorrect phone number format', HttpStatus.BAD_REQUEST)
+    const userData = await this.prisma.user.findUnique({
+      where: { phone: userDto.phone },
+    })
+    const { name, surname, dateOfBirth, balance } = userData
+    const dateOfBirthString = dateOfBirth.toDateString()
+    return { name, surname, dateOfBirthString, balance }
+  }
+}
